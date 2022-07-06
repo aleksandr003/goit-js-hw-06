a011846d030e32343cb637b8f100acdf7d4dc162
aleksandr003 <herasymenko.oleks@gmail.com>
2022-07-06 15:10:57 +0300

    make 6 task

--- js/task-03.js
+++ js/task-03.js
@@ -1,14 +1,33 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
+
+const galleryListEl = document.querySelector(".gallery");
+
+// Функція для створення карточки makeGalleryCard(cardInfo)
+const makeGalleryCard = ({ url, alt } = {}) => {
+  return `
+    <li class="gallery-item">
+      <img class="gallery-img" src="${url}" alt="${alt}">
+    </li>
+  `;
+};
+
+// Створення масиву рядків із елементами
+const galleryCardsArr = images.map((el) => {
+  return makeGalleryCard(el);
+});
+
+// Вставка елементів на сторінку
+galleryListEl.insertAdjacentHTML("afterbegin", galleryCardsArr.join(""));
---
+++ js/task-04.js
@@ -0,0 +1,20 @@
+const btnDecrementEl = document.querySelector(
+  'button[data-action="decrement"]'
+);
+const btnIncrementEl = document.querySelector(
+  'button[data-action="increment"]'
+);
+const valueEl = document.querySelector("#value");
+const counterValue = 0;
+
+const addOneNumber = function () {
+  counterValue += 1;
+  valueEl.textContent = counterValue;
+};
+const removeOneNumber = function () {
+  counterValue -= 1;
+  valueEl.textContent = counterValue;
+};
+
+btnIncrementEl.addEventListener("click", addOneNumber);
+btnDecrementEl.addEventListener("click", removeOneNumber);
